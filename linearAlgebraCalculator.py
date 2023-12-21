53b37cc3384e353c2670349798ce72df69118273
Objective Axioma <objective.axioma@gmail.com>
2023-12-21 17:30:23 -0500

    outline of program

---
+++ linearAlgebraCalculator.py
@@ -0,0 +1,51 @@
+'''
+    This calculator will have the following features:
+    * add a constant to a matrix
+    * multiply a matrix by a constant
+    * add two matricies
+    * multiple two compatible matrices
+    ** tell if two matrices are compatible for addition and/or multiplication
+    * tell if a matrix is invertible
+    * invert an invertible matrix
+    ** check first to see if it is invertible
+    * solve a system of linear equations
+    ** Cramer's method
+    ** backward substitution if applicable
+    ** forward substitution if applicable
+    * keep track of operation count
+'''
+
+OP_COUNT = 0;
+
+def getMatrix():
+    return 0;
+
+def addConstant(A, c):
+    return 0;
+
+def multConstant(A, c):
+    return 0;
+
+def addMatrix(A, B):
+    return 0;
+
+def multMatrix(A, B):
+    return 0
+
+def checkAddCompatible(A, B):
+    return 0;
+
+def checkMultCompatible(A, B):
+    return 0;
+
+def checkInvertible(A):
+    return 0;
+
+def CramersMethod(A, b):
+    return 0;
+
+def backwardSub(A, b):
+    return 0;
+
+def forwardSub(A, b):
+    return 0;
